563f017498e6839d6ec825cac0b2e169bcbd7ec7
Bernardo Vale <bernardosilveiravale@gmail.com>
2016-12-04 23:49:45 -0200

    Update credentials.sh

--- credentials.sh
+++ credentials.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Place your credentials here
-export AWS_ACCESS_KEY_ID="AKIAJZ766BSOA5JKXIEQ"
-export AWS_SECRET_ACCESS_KEY="Wo2LfJG+xq6DMotl3Axkmi86zgIcR+R9l23KU4+M"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
